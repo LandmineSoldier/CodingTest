6675f040c1571ccafdd4fbd10e41f9f5b3a26b51
이호경 <61589535+LandmineSoldier@users.noreply.github.com>
2021-05-15 19:26:37 +0900

    Create A3-20210515-.c

---
+++ BaekJoon/C/A6-Character/A3-20210515-.c
@@ -0,0 +1,25 @@
+/*
+ * 2675 : 문자열 반복
+ * 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오.
+ * 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+ * QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
+ */
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    int inMax;
+    scanf("%d", &inMax);
+    for (int i = 0; i < inMax; i++)
+    {
+        int times;
+        char T[1000];
+        scanf("%d %s", &times, T);
+        for (int j = 0; j < strlen(T); j++)
+        {
+            for (int k = 0; k < times; k++)
+                printf("%c", T[j]);
+        }
+        puts("");
+    }
+}
